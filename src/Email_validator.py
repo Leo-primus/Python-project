02fcf033d5834fbaf33186ca7f5060f323c8695c
Leo Latsch <leostefan20080208@gmail.com>
2025-10-31 07:20:41 +0100

    Made a working Email_validator and a fake creator

--- src/Email_validator.py
+++ src/Email_validator.py
@@ -2,22 +2,23 @@ import re
 #def get_user_input(string_to_display):
     #a = input(string_to_display).strip()
     #return a 
+data_path_c = "./data/invalid_email.csv"
 data_path_b = "./data/valid_email.csv"
 data_path = "./data/email.csv"
 def mail_checker(user_email):
     user_email1 = re.match(pattern=r"(\w+\.?)*\+?\w+\@\w+\.\w+",string=str(user_email))
     if user_email1 is None:
         print("it didnt match")
-        with open(data_path_b,'a') as csv:
-         n = csv.write(f'{user_email} \n')   
+        with open(data_path_c,'a') as csv:
+         n = csv.write(f'{user_email}')   
     else:
         print("It has matched")
-        with open(data_path,'a') as csv:
-            y = csv.write(f'{user_email} \n')
+        with open(data_path_b,'a') as csv:
+            y = csv.write(f'{user_email}')
         
 
 with open(data_path,'r') as csv:
     user_email = csv.readlines()
     for user_email in user_email:
         mail_checker(user_email)
-        print(user_email)               
+        print(user_email)                  
